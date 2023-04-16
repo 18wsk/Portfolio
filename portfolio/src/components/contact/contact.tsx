8026facc4112f6ba54045cd02cc63a5ba3883b46
willkennedy2000 <william.kennedy@carbongraph.io>
2023-04-16 17:45:18 -0400

    fixed email bug

--- portfolio/src/components/contact/contact.tsx
+++ portfolio/src/components/contact/contact.tsx
@@ -14,12 +14,13 @@ const Contact = () => {
     const sendEmail = async () => {
         if (formData.name !== "" && formData.message !== "" && formData.contact !== "") {
             await emailjs.send('service_nr375yv', 'template_dldwu8r', formData, "uAZqooKamZXIcA_qD")
-            .then(function(response) {
+            .then(function() {
                 toast("success");
-            }, function(error) {
+            }, function() {
                 toast("Could not send email, please try again!")
             });
         } else {
+            console.log(formData)
             toast("Please fill all the fields!")
         }
     }
@@ -30,15 +31,15 @@ const Contact = () => {
             <div className="md:w-1/2 xs:w-full rounded-xl shadow-2xl md:p-10 xs:px-4">
                 <h1 className="text-xl font-bold text-left p-1">Name:</h1>
                 <div className='w-full pb-2 flex justify-center'>
-                    <input type="text" placeholder="Name" className="xs:w-full md:w-3/4 h-12 rounded-xl bg-gray-100 px-2" required onChange={(e) => setFormData({...formData, name: e.target.value})}/>
+                    <input name="name" type="text" placeholder="Name" className="xs:w-full md:w-3/4 h-12 rounded-xl bg-gray-100 px-2" required onChange={(e) => setFormData({...formData, name: e.target.value})}/>
                 </div>
                 <h1 className="text-xl font-bold text-left p-1">Message:</h1>
                 <div className='w-full pb-2 flex justify-center'>
-                    <textarea placeholder="Message" className="xs:w-full md:w-3/4 md:min-h-40 xs:h-20 rounded-xl bg-gray-100 px-2" required onChange={(e) => setFormData({...formData, name: e.target.value})}></textarea>
+                    <textarea name="message" placeholder="Message" className="xs:w-full md:w-3/4 md:min-h-40 xs:h-20 rounded-xl bg-gray-100 px-2" required onChange={(e) => setFormData({...formData, message: e.target.value})}></textarea>
                 </div>
                 <h1 className="text-xl font-bold text-left p-1">How to Reach You:</h1>
                 <div className='w-full pb-2 flex justify-center'> 
-                    <input type="text" placeholder="Leave preferred contact please" className="xs:w-full md:w-3/4 h-12 rounded-xl bg-gray-100 px-2" required onChange={(e) => setFormData({...formData, name: e.target.value})}/>
+                    <input name="contact" type="text" placeholder="Leave preferred contact please" className="xs:w-full md:w-3/4 h-12 rounded-xl bg-gray-100 px-2" required onChange={(e) => setFormData({...formData, contact: e.target.value})}/>
                 </div>
                 <div className="w-full flex justify-center py-4">
                     <button className="p-2 rounded-xl bg-my-blue text-white hover:bg-blue-500 w-1/3" onClick={sendEmail}>Send</button>
